f2751f2c191261707538d73847b8c6b110df3e7b
JanneP80 <janne.puolitaival@student.saimia.fi>
2017-02-17 14:20:54 +0200

    More exercises

---
+++ loops-t3-t5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loops_t3_t5 // t3 only positive numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // program sums even and odd N numbers.
+
+            int n = 0;            
+            int evensum = 0;
+            int oddsum = 1;
+            string message = string.Empty;
+            Console.Write("Please type amount of numbers for calculating odd and even sums: ");
+
+            bool userinput = int.TryParse(Console.ReadLine(), out n);
+            if (!userinput) // incorrect input
+            {
+                message = "Input is NOT a number.";
+            }
+
+            else // do calculation
+            {
+                Console.Write("Calculating even and odd number sum: ");
+                for (int i = 0; i < n + 1; i++)
+                {
+                    Console.Write(" {0}", i); // error check line
+                    evensum = evensum + i;
+                    oddsum = oddsum + i-1;
+                    i++;
+                }
+
+
+                message = string.Format("You gave {0} numbers to sum. \nThe evensum is {1} and oddsum is {2}", n, evensum, oddsum);
+            }
+
+            Console.WriteLine("\n" + message);
+            Console.ReadKey();
+        }
+    }
+}
